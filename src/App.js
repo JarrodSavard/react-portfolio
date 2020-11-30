9c29b8d3b5275eb0bd4cbe19c97079574a78ac9a
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-11-30 14:45:50 -0800

    Imported Route & Switch from react-router-dom // Added Route for bout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// Header Component
 import Header from './components/header/header.component';
+// Pages
+import About from './pages/about/about.component';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Switch>
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
     );
   }
