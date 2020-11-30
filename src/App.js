a78935285728e4774fd28c71b922498af41da897
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-11-30 14:29:53 -0800

    whiped styling and reformated file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+// import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Header from './components/header/header.component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <></>
+      <div>
+        <Header />
       </div>
     );
   }
