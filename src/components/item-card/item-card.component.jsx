a267c366259e2f45c01999416282a5d7a8b4e9f6
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-12-14 03:56:27 -0800

    removed id, added link

--- src/components/item-card/item-card.component.jsx
+++ src/components/item-card/item-card.component.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './item-card.styles.scss';
+import { Link } from 'react-router-dom';
 
-const ItemCard = ({ id, name, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      
+const ItemCard = ({ link, name, imageUrl }) => (
+  <a href={link} target="_blank">
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 export default ItemCard;
