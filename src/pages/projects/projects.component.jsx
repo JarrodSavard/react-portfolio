2ce2c46ec095680399b10188b3ea6ee8a7f5c84c
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-12-17 00:10:25 -0800

    addded to projects page

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
-import './projects.styles.scss'
+import './projects.styles.scss';
+import ProjectCard from '../../components/project-card/project-card.components';
+
+import TestIMG from '../../assets/images/project-bg-one.jpg';
+const ProjectPage = (props) => (
+  <div>
+    <ProjectCard name="Typing Test" imageUrl={TestIMG} link="" />
+  </div>
+);
+
+export default ProjectPage;
