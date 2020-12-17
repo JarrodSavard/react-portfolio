6f11d0abfc4b79e5702917e6678a58ad6fcd4af4
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-12-16 19:28:45 -0800

    changed img to h2 tag and reformated code

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss';
+import LOGO from '../../assets/images/API_logo.png';
 
 const Header = (props) => (
   <div className="header">
     <Link className="logo-container" to="/about">
-      <p>IMG</p>
+      <h2>Jarrod Savard</h2>
     </Link>
     <div className="options">
       <Link className="option" to="/about">
